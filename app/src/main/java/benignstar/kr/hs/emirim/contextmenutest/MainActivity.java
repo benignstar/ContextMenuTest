faffb129cb1dc11926d05c04eb1760d92b645a0a
benignstar <che0009@naver.com>
2017-05-25 18:34:05 +0900

    #3 callback Method

--- app/src/main/java/benignstar/kr/hs/emirim/contextmenutest/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/contextmenutest/MainActivity.java
@@ -2,12 +2,38 @@ package benignstar.kr.hs.emirim.contextmenutest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but1=(Button)findViewById(R.id.but1);
+        Button but2=(Button)findViewById(R.id.but2);
+        registerForContextMenu(but1);
+        registerForContextMenu(but2);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater=getMenuInflater();
+        if(v.getId()==R.id.but1){
+            menu.setHeaderTitle("Background Color");
+            menuInflater.inflate(R.menu.menu1, menu);
+            menu.setHeaderIcon(R.drawable.santaclaus);
+        }
+        if(v.getId()==R.id.but2){
+            menuInflater.inflate(R.menu.menu2, menu);
+        }
+    }
+
+
 }
